e3694f8621a38b9f6d4688c0c1ee05345d2cc012
cDots Technologies LLC <44267543+CDOTSTECH@users.noreply.github.com>
2024-09-23 22:41:45 -0500

    Update and rename puzipper.py to pyzipper.py

--- pyzipper.py
+++ pyzipper.py
@@ -8,3 +8,14 @@ def extract_zip_with_password(zip_filename, password, extract_to):
 
 # Example usage
 extract_zip_with_password('protected.zip', 'your_password', 'extracted_files')
+
+---------------
+import gzip
+import shutil
+
+def extract_gzip(gzip_filename, extract_to):
+    with gzip.open(gzip_filename, 'rb') as f_in:
+        with open(extract_to, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+extract_gzip('large_file.gz', 'extracted_file')
